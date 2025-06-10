c7a491a44c883051dd592b99bb22cf2e14c7e655
Fujiwara <te220320@gmail.com>
2025-06-10 11:11:29 +0900

    commit

--- src/main/java/jp/te4a/spring/boot/myapp12/mybootapp12/BookForm.java
+++ src/main/java/jp/te4a/spring/boot/myapp12/mybootapp12/BookForm.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp11.mybootapp11;
+package jp.te4a.spring.boot.myapp12.mybootapp12;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -15,6 +15,7 @@ public class BookForm {
     @TestValid(param="abc")
     private String title;
     @Size(min=3,max=20)
+    @Writter(ok="東北タロウ")
     private String writter;
     private String publisher;
     @Min(0)
---
+++ src/main/java/jp/te4a/spring/boot/myapp12/mybootapp12/WritterValidator.java
@@ -0,0 +1,32 @@
+package jp.te4a.spring.boot.myapp12.mybootapp12;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class WritterValidator implements ConstraintValidator<Writter, String> {
+
+    private String ok;
+
+    @Override
+    public void initialize(Writter annotation) {
+        this.ok = annotation.ok();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        // okの値と比較する等、バリデーションロジックを書く
+        if (value == null) {
+            return true;  // nullはバリデーションしない場合
+        }
+
+        if (!ok.equals(value)) {
+            // デフォルトメッセージを使うために一旦disableしてから再設定
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Input " + value)
+                   .addConstraintViolation();
+            return false;
+        }
+
+        return true;
+    }
+}
